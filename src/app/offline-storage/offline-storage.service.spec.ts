e5a27cc8c7d55cff55ba759f25725cdd7b8d45b3
Caleb Pruitt <cpruitt@worcester.edu>
2018-05-01 12:33:30 -0400

    Made some Changes with Promises but the delete test might be broken

--- src/app/offline-storage/offline-storage.service.spec.ts
+++ src/app/offline-storage/offline-storage.service.spec.ts
@@ -6,58 +6,121 @@ import { OfflineStorageService } from './offline-storage.service';
 describe('OfflineStorageService', () => {
   let service = new OfflineStorageService();
   let patient1 = PATIENTS[0];
+  let pat1ID = patient1.fullName + patient1.ampathMrsUId;
   let patient2 = PATIENTS[1];
+  let pat2ID = patient2.fullName + patient2.ampathMrsUId;
   let patient3 = PATIENTS[2];
+  let pat3ID = patient1.fullName + patient1.ampathMrsUId;
   beforeEach(() => {
-    /*if (service.db != null) {
-      service.clear();
-    }*/
+    service.clear();
     TestBed.configureTestingModule({
       providers: [OfflineStorageService]
     });
   });
 
-  it('Should instantiate a PouchDB database',
+  it('Should return a pouch instance',
     inject([OfflineStorageService], () => {
-      service.createDB('test');
+      service.createDB('http://localhost:5984/test');
       expect(service).not.toBeNull();
     })
   );
 
-  it('Should return a pouch instance',
+  it('Should show 0 documents for newly created pouch instance',
     inject([OfflineStorageService], () => {
-      service.createDB('test1');
-      expect(service.db instanceof PouchDB).toBeTruthy();
-    })
-  );
+      service.createDB('http://localhost:5984/test');
+      service.docCount().then((result) => {
+        console.log(result);
+        expect(result).toBe(0);
+      }).catch((err) => {
+        console.log(err);
+        fail(err);
+      });
+    }));
 
   it('Should be able to add a patient to the PouchDB database',
     inject([OfflineStorageService], () => {
-      service.createDB('test3');
-      service.addDoc(patient2).then((response) => {
-        console.log(response);
+      service.createDB('http://localhost:5984/test');
+      service.addDoc(pat2ID, patient2).then((response) => {
         expect(response.ok).toBe(true);
       });
     })
   );
 
-  it('Should be able to add multiple patients',
+  it('Should be able to update patient data',
     inject([OfflineStorageService], () => {
-    service.createDB('test4');
-    expect(service.addDoc(patient1)).toBeTruthy();
-    expect(service.addDoc(patient2)).toBeTruthy();
-    expect(service.addDoc(patient3)).toBeTruthy();
+      service.createDB('http://localhost:5984/test');
+      service.addDoc(pat1ID, patient2);
+      service.addDoc(pat1ID, patient1).then((response) => {
+        console.log(response);
+        expect(response.ok).toBe(true);
+      }).catch((err) => {
+        console.log(err);
+        fail();
+      });
+    })
+  );
+
+  it('Should be able to clear the database after items are added',
+    inject([OfflineStorageService], () => {
+      service.createDB('http://localhost:5984/test');
+      service.addDoc(pat1ID, patient1);
+      service.addDoc(pat2ID, patient2);
+      service.clear();
+      service.docCount().then((response) => {
+        console.log(response);
+        expect(response).toBe(0);
+      }).catch((err) => {
+        console.log(err);
+      });
+    }));
+
+  it('Should be able to add multiple patients Test1',
+    inject([OfflineStorageService], () => {
+      service.createDB('http://localhost:5984/test');
+      service.addDoc(pat1ID, patient1);
+      service.addDoc(pat2ID, patient2).then((result) => {
+        service.docCount().then((response) => {
+          console.log(response);
+          expect(response).toBe(2);
+        }).catch((err) => {
+          console.log(err);
+          fail();
+        });
+        expect(result).toBe(true);
+      }).catch((err) => {
+        console.log(err);
+        fail();
+      });
     })
   );
 
   it('Should discard all patient data with clear()',
     inject([OfflineStorageService], () => {
-      service.createDB('test5');
-      service.addDoc(patient1);
-      service.addDoc(patient2);
-      service.addDoc(patient3);
-      expect(service.clear()).toBeTruthy();
+      service.createDB('http://localhost:5984/test');
+      service.addDoc(pat1ID, patient1);
+      service.addDoc(pat2ID, patient2);
+      service.addDoc(pat3ID, patient3);
+      service.clear();
+      service.docCount().then((response) => {
+        console.log(response);
+        expect(response).toBe(0);
+      }).catch((err) => {
+        console.log(err);
+        fail();
+      });
     })
   );
 
+  it('Should delete item from DB',
+    inject([OfflineStorageService], () => {
+      service.createDB('http://localhost:5984/test');
+      service.addDoc(pat1ID, patient1);
+      service.deleteDoc(pat1ID).then((response) => {
+        expect(response).toBe(true);
+      }).catch((err) => {
+        console.log(err);
+        fail();
+      });
+    })
+  );
 });
--- src/app/offline-storage/offline-storage.service.ts
+++ src/app/offline-storage/offline-storage.service.ts
@@ -1,67 +1,108 @@
 import { Injectable, InjectionToken } from '@angular/core';
 import PouchDB from 'pouchdb';
 
-interface PouchDBPutResult {
-  ok: boolean;
-  id: string;
-  rev: string;
-}
-
 @Injectable()
 export class OfflineStorageService {
-  public dbname: string;
+  public dbname: string = null;
   private db: PouchDB = null;
 
   constructor() {
   }
 
   public createDB(name: string): void {
+    console.log('Creating PouchDB instance: ' + name);
     this.db = new PouchDB(name);
-    console.log(this.db.adapter);
   }
 
-  public addDoc(jsonObject: any): Promise<any> {
-    console.log('checking if object already exists');
-    return this.db.get(jsonObject._id).then((existing) => {
+  public addDoc(id: string, jsonObject: any): Promise<any> {
+    return this.db.get(id).then((existing) => {
+      console.log('checking if object already exists');
+      console.log(existing);
       return this.db.put({
-        _id: existing.id,
-        _rev: existing.rev,
-        data: jsonObject
+        _id: id,
+        _rev: existing._rev,
+        jsonObject
+      }).then((response) => {
+        return response.ok;
+      }).catch((err) => {
+        console.log(err);
       });
     }).catch((doesntExist) => {
-      console.log('Storing captured data for the first time:', jsonObject._id);
-      return this.db.put(jsonObject);
+      return this.db.put({
+        _id: id,
+        jsonObject
+      }).then((response) => {
+        console.log('Storing captured data for the first time: ' + response.id);
+        console.log(response);
+        return response;
+      }).catch((err) => {
+        console.log(err);
+      });
     });
   }
 
-  public getDoc(id: string): JSON {
-    let x1: JSON;
-    this.db.get(id).then((doc) => {
-      x1 = doc;
+  public getDoc(id: string): Promise<any> {
+    return this.db.get(id).then((existing) => {
+      console.log('document exists');
+      console.log(existing);
+      return existing;
     }).catch((err) => {
       console.log(err);
-    });
-    return x1;
-  }
-
-  public deleteDoc(id: string): JSON {
-    let element: JSON;
-    this.db.remove(id).then((doc) => {
-      element = doc;
-      console.log(doc);
-    });
-    return element;
-  }
-
-  public clear(): boolean {
-    let success: boolean = false;
-    return this.db.destroy().then((response) => {
-     console.log(response);
-     success = true;
-     return success;
-    }).catch((err) => {
-      console.log(err);
+      return null;
     });
   }
 
+  public docCount(): Promise<any> {
+    if (this.db != null) {
+      return this.db.info().then((response) => {
+        /*console.log(response);*/
+        return response.doc_count;
+      }).catch((err) => {
+        console.log(err);
+      });
+    } else {
+      console.log('DB is NULL');
+      return new Promise((result) => {
+        console.log('Empty DB, returning 0');
+        return 0;
+      }).catch((err) => {
+        console.log('Something broke');
+        console.log(err);
+      });
+    }
+  }
+
+  public deleteDoc(id: string): Promise<any> {
+    return this.db.get(id).then((existing) => {
+      console.log(existing);
+      console.log('deleting data with _id: ' + id);
+      return this.db.put(existing).then((doc) => {
+        doc._deleted = true;
+        return this.db.put(doc);
+      }).catch((err) => {
+        console.log(err);
+      });
+    }).catch((error) => {
+      console.log('No data found for _id: ' + id);
+    });
+  }
+
+  public clear(): Promise<any> {
+    if (this.db != null) {
+      return this.db.destroy().then((response) => {
+        console.log('Clearing data from PouchDB');
+        console.log(response);
+        return response.ok;
+      }).catch((err) => {
+        console.log(err);
+        return err;
+      });
+    } else {
+      return new Promise<any>((result) => {
+        return false;
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+  }
 }
