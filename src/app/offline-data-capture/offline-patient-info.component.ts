92ac79e674f24debee21ccfb77abc141eefc5d22
Jason Knowles <jknowles.cs@gmail.com>
2018-05-03 07:28:34 -0400

    Some basic patient info from PouchDB is now being displayed properly in the offline-patient-info component.

--- src/app/offline-data-capture/offline-patient-info.component.ts
+++ src/app/offline-data-capture/offline-patient-info.component.ts
@@ -8,16 +8,23 @@ import { OfflinePatientInfoService } from './offline-patient-info.service';
 })
 export class OfflinePatientInfoComponent implements OnInit, OnDestroy {
 
-  // public patient: any;
+  public patient: any;
   public patientLoaded: boolean = false;
   constructor(private _offlinePatientInfoService: OfflinePatientInfoService) {
   }
-
   public getPatient() {
-    let patient: any[] = this._offlinePatientInfoService.getPatient(
-      'patient-064e419c-ff4e-4a6f-b83f-e1df48e80723');
-    this.patientLoaded = true;
-    console.log('component - result', patient);
+    return this._offlinePatientInfoService.getPatient(
+      'patient-064e419c-ff4e-4a6f-b83f-e1df48e80723').then((result) => {
+      this.patientLoaded = true;
+      this.patient = result.patient;
+      console.log('component result:', result);
+      console.log('component patient:', this.patient);
+      // console.log('component - patient.patient', patient.patient);
+    }).catch((notExisting) => {
+      console.log('getPatient(): UUID not found in PouchDB database.');
+    });
+    // console.log('component outside promise - patient:', patient);
+    // console.log('component outside promise - patient.patient', patient.patient);
   }
 
   public ngOnInit() {
